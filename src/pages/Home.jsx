1df3bc7e63a3e149f61431d981058f8089879b8c
Karthik <karthikeyareddy122@gmail.com>
2025-04-14 18:38:39 -0500

    home wallpaper change

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,7 +1,7 @@
 import { Link } from 'react-router-dom';
 import React, { useEffect, useState } from "react";
 import "../styles/Home.css";
-import heroImg from "../assets/lotus.jpg";
+import heroImg from "../assets/lotus_wallpaper.jpeg";
 import client1 from "../assets/testimonials/client1.jpg";
 import client2 from "../assets/testimonials/client2.jpg";
 import client3 from "../assets/testimonials/client3.jpg";
