429be70ddce44419552d6f0e19255e0469165541
Karthik <karthikeyareddy122@gmail.com>
2025-04-15 00:09:01 -0500

    abot us page changes

--- src/pages/About.jsx
+++ src/pages/About.jsx
@@ -1,8 +1,8 @@
 
 import '../styles/About.css';
-import founderImg from '../assets/founders/founder.jpg';
+import founderImg from '../assets/founders/sneha.jpeg';
 import cofounderImg from '../assets/founders/Shwetha.jpeg';
-import about from '../assets/hero.jpg';
+import about from '../assets/lotus_about6.jpeg';
 
 function About() {
   return (
@@ -10,7 +10,7 @@ function About() {
       {/* Hero Section */}
       <section className="about-hero-section" style={{ backgroundImage: `url(${about})` }}>
         <div className="about-hero-overlay">
-          <h1 data-aos="zoom-in">About Us</h1>
+        <h1 data-aos="zoom-in" style={{ textAlign: 'left' }}>About Us</h1>
          {/*}
          <p data-aos="fade-up">
           Divine aesthetics and timeless elegance — curated in every style. <br />
@@ -41,8 +41,16 @@ function About() {
           </div>
           <div className="founder-text" data-aos="fade-left">
             <h2>Sneha Gorijala</h2>
-            <p class="tagline" style={{ fontSize: '22px' }}>Founder and Creative Head</p>
-            <p>"Design is the silent ambassador of elegance."</p>
+            <p class="tagline" style={{ fontSize: '22px' }}>Founder, CEO and Creative Head</p>
+            <p>As someone born and raised in India, tradition has always been at the heart of who I am. When I moved to the U.S., I brought that passion with me—not just in memories, but in spirit,
+ and celebration. Blending the richness of Indian culture with the elegance of modern design felt natural, 
+ and it’s what led us to create Lotus Events & Decor.</p>
+            <p> With two daughters and a full life, I’ve worn many hats over the years—mother, entrepreneur, creative director, and problem-solver. But above all, I’m someone who finds purpose in creating joy for others. 
+              Designing events isn’t just about aesthetics for me—it’s about telling stories, honoring culture, and making people feel truly seen and celebrated.</p>
+            <p> What began as a dream has grown into something much more: a business rooted in heart, authenticity, and care. 
+              I pour that same energy into every celebration we take on, ensuring that no matter the budget or size, the result is unforgettable.</p>
+              <p> For me, Lotus isn’t just a business—it’s a reflection of everything I love: family, culture, beauty, and connection. 
+                Being able to bring people together through meaningful, memorable experiences is more than a job—it’s a blessing I’m grateful for every day.</p>
           </div>
         </div>
 
